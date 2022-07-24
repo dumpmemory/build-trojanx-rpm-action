9305e70ece5b97785ced39468d8c17a307539d5e
兩足大貓貓 <14369594+M1Screw@users.noreply.github.com>
2022-07-25 05:06:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,39 +2,25 @@
 
 set -eux
 
-INPUT_VERSION="$(echo "$INPUT_VERSION" | sed -E "s,^refs/tags/,,")"
-
 /replacetool \
-  --specfile:/template.spec \
-  --summary:"$INPUT_SUMMARY" \
-  --package-root:"$INPUT_PACKAGE_ROOT" \
-  --package:"$INPUT_PACKAGE" \
-  --version:"$INPUT_VERSION" \
-  --arch:"$INPUT_ARCH" \
-  --maintainer:"$INPUT_MAINTAINER" \
-  --vendor:"$INPUT_VENDOR" \
-  --description:"$INPUT_DESC" \
-  --license:"$INPUT_LICENSE"
+  --bin:"$bin" \
+  --spec:"$spec" \
+  --config:"$config" \
+  --service:"$service"
 
-readonly RPMBUILD_DIR="$HOME/rpmbuild"
-readonly RPMBUILD_SOURCE_DIR="$RPMBUILD_DIR/SOURCES"
-readonly RPMBUILD_SPEC_DIR="$RPMBUILD_DIR/SPECS"
+readonly rpmbuild_dir="$HOME/rpmbuild"
+readonly build_dir="$rpmbuild_dir/BUILD"
+readonly spec_dir="$rpmbuild_dir/SPECS"
+readonly rpm_dir="$rpmbuild_dir/RPMS"
 (
-  readonly WORKDIR="/tmp/work"
-  mkdir "$WORKDIR"
-  cp /template.spec "$WORKDIR"
-  cp -rp "$INPUT_PACKAGE_ROOT" "$WORKDIR/$INPUT_PACKAGE"
-  cd "$WORKDIR"
-  tar czf tmp.tar.gz "$INPUT_PACKAGE/"
+  cp "$bin" "$build_dir"
+  cp "$config" "$build_dir"
+  cp "$service" "$build_dir"
 
-  mkdir -p "$RPMBUILD_SOURCE_DIR"
-  mkdir -p "$RPMBUILD_SPEC_DIR"
-  mv tmp.tar.gz "$RPMBUILD_SOURCE_DIR"
-
-  cp -p template.spec "$RPMBUILD_SPEC_DIR"
-  rpmbuild -bb "$RPMBUILD_SPEC_DIR/template.spec"
+  cp -p "$spec" "$spec_dir"
+  rpmbuild -bb "$spec_dir/$spec"
 )
 
-cp -p "$RPMBUILD_DIR/RPMS/$(uname -m)"/*.rpm .
+cp -p "$rpm_dir"/*.rpm .
 
 ls -lah ./*.rpm
